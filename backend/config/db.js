ff9b310e3f2f128e62ba4526f7d72527d15ee9b3
jay bodra <jaybodra01@gmail.com>
2025-01-13 13:42:54 +0530

    backend api created and call it

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const seedDatabase = require('../sampleData');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/quiz-app'
+);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    
+    // Force reseed the database
+    await seedDatabase();
+    
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB; 
